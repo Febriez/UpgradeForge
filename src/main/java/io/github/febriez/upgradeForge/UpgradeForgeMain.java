de4cd8b2d62743d03d1b5da48c678f13bf252e0a
Febriez <febrie@naver.com>
2024-10-09 07:31:25 +0900

    First Commit

---
+++ src/main/java/io/github/febriez/upgradeForge/UpgradeForgeMain.java
@@ -0,0 +1,17 @@
+package io.github.febriez.upgradeForge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UpgradeForgeMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
